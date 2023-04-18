1cfba318ef601b07a48789db4443fbb74667edbf
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-17 20:41:36 -0400

    Update pdf.jsx
    Cc

--- components/pdf.jsx
+++ components/pdf.jsx
@@ -311,7 +311,7 @@ useEffect(() => {
                 </Page>
             </Document>}
 fileName='Cotizacion Terrestre'>
-<Button style={'buttonPrimary'} click={}>pdf</Button>
+<Button style={'buttonPrimary'}>pdf</Button>
 </PDFDownloadLink>}
         </div>
     )
