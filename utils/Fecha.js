452cfde73e35a4b0433feaa760b3002bbedb7eef
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-17 22:01:50 -0400

    Update Fecha.js
    Cc

--- utils/Fecha.js
+++ utils/Fecha.js
@@ -5,7 +5,7 @@ function getDayMonthYear (setUserDayMonthYear) {
     const days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31',]
 
 
-    const date = new Date('Sun Apr 9 2023 16:05:28 GMT-0400 (hora de Bolivia)');
+    const date = new Date();
     
     return `${days[date.getDate()-1]}-${months[date.getMonth()]}-${date.getUTCFullYear()}`
     
